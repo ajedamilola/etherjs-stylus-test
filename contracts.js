7e7cc511252b82abaf0fe4cdf0347036659e2671
Aje Damilola <ajedamilola2005@gmail.com>
2024-09-21 23:23:09 +0100

    Yhhh

--- contracts.js
+++ contracts.js
@@ -5,22 +5,24 @@ import { JsonRpcProvider, JsonRpcSigner } from "ethers";
 import { Wallet } from "ethers";
 
 async function main() {
-    let contractAddress = "0x35bc8D3D5041383DCee24F9cC0053b7F24c906F1";
+    let contractAddress = "0xea9a0bb706c3f6701a5c7ad78f0663eef638ddd6";
     let ABI = [
-        "function number() external view returns (uint256)",
-        "function setNumber(uint256 number) external",
-        "function increment() external",
-        "function decrement() external"
+        "function pushMessage(string calldata new_message) external",
+        "function data() external view returns (string memory)",
     ]
 
     let provider = new JsonRpcProvider(data.rpc)
     // let signer = await provider.getSigner()
-    const wallet = new Wallet("Your secret key here", provider);
+    const wallet = new Wallet("private key", provider);
     let contract = new Contract(contractAddress, ABI, wallet)
-    // const change = await contract.setNumber(BigInt(22))
-    const number = await contract.number();
-    // console.log(change)
-    console.log(number)
+    const database = JSON.parse(await contract.data());
+    // database.messages.push({
+    //     name: "Aje Damilola",
+    //     content: "I really dont like rust at all",
+    //     sender: "0x654e9d96e6EF0405a49740C5F329E0c01a305322"
+    // })
+    // await contract.pushMessage(JSON.stringify(database))
+    console.log(database)
 }
 
 main();
